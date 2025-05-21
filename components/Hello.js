1fc49938255a1779edcf4e1f79617897d61bcd62
Tsvetomir Yotov <yotovtsvetomir@gmail.com>
2025-05-21 14:47:51 +0300

    Update Hello.js

--- components/Hello.js
+++ components/Hello.js
@@ -16,7 +16,7 @@ const Hello = () => (
 						I am Full Stack Web developer able to build Web presence from the ground up - from concept, navigation,
 						layout and programming to UX and SEO. Skilled at writing well-designed, testable and efficient code using
 						current best practices in Web development. Fast learner, hard worker and team player who is proficient in
-						an array of scripting languages and multimedia Web tools.
+						an array of programming languages, devOps and cloud systems.
 						</p>
 						<div className="hello_actions">
 							<a href="../assets/yotovCV.pdf" className="btn"><Download size={20} color="#fff" /> Download CV</a>
